82f90c5f4cc976b2bf2a10f3c7bf1a41c7031a6a
kkm4512 <145191380+kkm4512@users.noreply.github.com>
2024-01-19 19:41:27 +0900

    first
    first

---
+++ Test.main.js
@@ -0,0 +1,69 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions, 
+    ],
+});
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+const Roles = async (message, roleId) => {
+    try {
+        const role = await message.guild.roles.fetch(roleId);
+        if (role) {
+            await message.member.roles.add(role);
+            message.react('✅');
+            return true;
+        } else {
+            await message.react('❌');
+            message.reply('역할을 찾을 수 없습니다.');
+        }
+    } catch (error) {
+        console.error(error);
+        await message.react('⚠️');
+        message.reply('역할을 추가하는 중에 문제가 발생했습니다.');
+    }
+    return false;
+};
+
+const ALL = async (lists, message, WhatTear1,RoleCode) => {
+    if (message.channel.name === 'ع˖⁺-⋆｡⋆༶역할-부여'){
+        return await Roles(message, RoleCode);
+    }
+    return false;
+};
+
+client.on('messageCreate', async message => {
+    console.log(message.channel.name)
+    if (message.author.bot) return;
+    let lists = message.content.split(' ');
+    lists = lists.filter(item => item !== '');
+
+    let roleAssigned = false;
+    if (await ALL(lists, message, 'U', '1178874545550143598')) roleAssigned = true;
+    else if (await ALL(lists, message, 'I',  '1178867058021257256')) roleAssigned = true;
+    else if (await ALL(lists, message, 'B',  '1178869859103293440')) roleAssigned = true;
+    else if (await ALL(lists, message, 'S',  '1178869850475602011')) roleAssigned = true;
+    else if (await ALL(lists, message, 'G',  '1178869785740705842')) roleAssigned = true;
+    else if (await ALL(lists, message, 'P',  '1178869784423698544')) roleAssigned = true;
+    else if (await ALL(lists, message, 'E',  '1178869782142009344')) roleAssigned = true;
+    else if (await ALL(lists, message, 'D',  '1178869683982704672')) roleAssigned = true;
+    else if (await ALL(lists, message, 'M',  '1178869682816688128')) roleAssigned = true;
+    else if (await ALL(lists, message, 'GM', '1178869681415802990')) roleAssigned = true;
+    else if (await ALL(lists, message, 'C',  '1178869672574210148')) roleAssigned = true;
+    
+    if (!roleAssigned) {
+        const replyMessage = await message.reply(`"생년 닉넴#태그 성별 티어 적어주시면 역할 부여됩니다.
+        ex) 01 수 정#kr1 여 E"`);
+        await replyMessage.react('❌');               
+    }
+});
+
+client.login(token);
